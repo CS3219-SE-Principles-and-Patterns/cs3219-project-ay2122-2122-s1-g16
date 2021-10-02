5a7cecf1431106a5a0178ca9bf22a8b243b14d41
Auxin <dexterzhang430@hotmail.com>
2021-10-02 18:02:46 +0800

    Initialization of prep project using spring boot

---
+++ peerprep/src/main/java/org/cs3219/project/peerprep/PeerprepApplication.java
@@ -0,0 +1,13 @@
+package org.cs3219.project.peerprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerprepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerprepApplication.class, args);
+	}
+
+}
---
+++ peerprep/src/test/java/org/cs3219/project/peerprep/PeerprepApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cs3219.project.peerprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerprepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

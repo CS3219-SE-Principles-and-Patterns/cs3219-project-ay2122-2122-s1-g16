1d4e1f79d2050dbe93bb22f9bb2879b03b698d80
wyt-sonia <e0365980@u.nus.edu>
2021-10-13 05:03:13 +0800

    router

--- peerprep/src/peerprep_frontend/src/router/Route.js
+++ peerprep/src/peerprep_frontend/src/router/Route.js
@@ -1,38 +1,31 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-// components
+import Sidebar from "../component/navbar/SideBar";
+import UserBanner from "../component/UserBanner";
+import Footer from "../component/Footer";
 
-import AdminNavbar from "components/Navbars/AdminNavbar.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import HeaderStats from "components/Headers/HeaderStats.js";
-import FooterAdmin from "components/Footers/FooterAdmin.js";
-
-// views
-
-import Dashboard from "views/admin/Dashboard.js";
-import Maps from "views/admin/Maps.js";
-import Settings from "views/admin/Settings.js";
-import Tables from "views/admin/Tables.js";
+import Dashboard from "../view/loggedIn/Dashboard";
+import Profile from "../view/loggedIn/user/Profile";
+import History from "../view/loggedIn/user/History";
+import ContactUs from "../view/ContactUs";
 
 export default function Admin() {
   return (
     <>
       <Sidebar />
       <div className="relative md:ml-64 bg-blueGray-100">
-        <AdminNavbar />
-        {/* Header */}
-        <HeaderStats />
+        <UserBanner />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <Switch>
             <Route path="/admin/dashboard" exact component={Dashboard} />
-            <Route path="/admin/maps" exact component={Maps} />
-            <Route path="/admin/settings" exact component={Settings} />
-            <Route path="/admin/tables" exact component={Tables} />
+            <Route path="/admin/profile" exact component={Profile} />
+            <Route path="/admin/history" exact component={History} />
+            <Route path="/admin/contactUs" exact component={ContactUs} />
             <Redirect from="/admin" to="/admin/dashboard" />
           </Switch>
-          <FooterAdmin />
-        </div>
+          <Footer bg="" position="relative"/>
+        </div> 
       </div>
     </>
   );

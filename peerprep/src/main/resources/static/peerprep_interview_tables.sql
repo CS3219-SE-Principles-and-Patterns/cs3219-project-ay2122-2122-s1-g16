4d3d78776775bb9da6da62aaa57f3d37307be649
Auxin <dexterzhang430@hotmail.com>
2021-10-13 13:49:06 +0800

    set up mysql config

---
+++ peerprep/src/main/resources/static/peerprep_interview_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `interview_questions` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255),
+  `content` text,
+  `difficulty` int
+);
+
+CREATE TABLE `interview_solutions` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `question_id` bigint,
+  `content` text
+);
+
+CREATE TABLE `users_questions_history` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `user_id` bigint,
+  `question_id` bigint,
+  `user_answer` text,
+  `created_at` timestamp,
+  `deleted_at` timestamp
+);
+
+CREATE TABLE `users_roles_history` (
+  `id` bigint PRIMARY KEY AUTO_INCREMENT,
+  `user_id` bigint,
+  `role` int,
+  `created_at` timestamp,
+  `deleted_at` timestamp
+);
+
+ALTER TABLE `interview_solutions` ADD FOREIGN KEY (`question_id`) REFERENCES `interview_questions` (`id`);
+
+ALTER TABLE `users_questions_history` ADD FOREIGN KEY (`question_id`) REFERENCES `interview_questions` (`id`);

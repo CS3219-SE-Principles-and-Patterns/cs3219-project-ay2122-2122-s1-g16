fbfd35182772773cf43ba20af6649e656b5b7d0d
shuiyao-sg <shuiyao@u.nus.edu>
2021-10-24 21:20:07 +0800

    Implement mail service with Gmail SMTP

--- peerprep/src/main/java/org/cs3219/project/peerprep/controller/AuthenticationController.java
+++ peerprep/src/main/java/org/cs3219/project/peerprep/controller/AuthenticationController.java
@@ -1,7 +1,6 @@
 package org.cs3219.project.peerprep.controller;
 
 import lombok.AllArgsConstructor;
-import org.aspectj.bridge.IMessage;
 import org.cs3219.project.peerprep.common.api.CommonResponse;
 import org.cs3219.project.peerprep.common.utils.AuthenticationUtil;
 import org.cs3219.project.peerprep.model.dto.authentication.*;
@@ -10,27 +9,38 @@ import org.cs3219.project.peerprep.model.entity.UserGroup;
 import org.cs3219.project.peerprep.service.AuthenticationService;
 import org.cs3219.project.peerprep.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @RestController
 @RequestMapping(value = "/api/v1/account")
-@AllArgsConstructor
 @Validated
 public class AuthenticationController {
     @Autowired
     private final AuthenticationService authenticationService;
     @Autowired
     private final EmailService emailService;
+    @Autowired
+    private final AuthenticationUtil authenticationUtil;
+    @Value("${custom.email.enable}")
+    private boolean emailEnabled;
+
+    public AuthenticationController(AuthenticationService authenticationService, EmailService emailService, AuthenticationUtil authenticationUtil) {
+        this.authenticationService = authenticationService;
+        this.emailService = emailService;
+        this.authenticationUtil = authenticationUtil;
+    }
 
     @PostMapping(path = "/register")
-    public ResponseEntity<CommonResponse<RegistrationResponse>> register(@RequestBody @Valid RegistrationRequest request) {
+    public ResponseEntity<CommonResponse<RegistrationResponse>> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
         User user = authenticationService.createNewUser(new User(request.getEmail(),
                 request.getNickname(),
                 request.getPassword(),
@@ -44,15 +54,15 @@ public class AuthenticationController {
         );
 
         // send email
-        // TODO: uncomment once we figure out how to send email in prod env
-        /*
-        String url = AuthenticationUtil.generateActivationUrl(user.getEmail(), user.getActivationToken());
-        String subject = AuthenticationUtil.generateActivationEmailSubject();
+        String url = authenticationUtil.generateActivationUrl(user.getEmail(), user.getActivationToken());
+        String subject = authenticationUtil.generateActivationEmailSubject();
         String recepientEmail = user.getEmail();
-        String senderEmail = AuthenticationUtil.generateEmailSenderAddress();
-        String emailContent = AuthenticationUtil.buildActivationEmailContent(url);
-        emailService.send(subject, recepientEmail, senderEmail, emailContent);
-         */
+        String senderEmail = authenticationUtil.generateEmailSenderAddress();
+        String emailContent = authenticationUtil.buildActivationEmailContent(url);
+
+        if (emailEnabled) {
+            emailService.send(subject, recepientEmail, senderEmail, emailContent);
+        }
 
         CommonResponse<RegistrationResponse> commonResponse = new CommonResponse<>(HttpStatus.CREATED.value(), "created", resp);
         return new ResponseEntity<>(commonResponse, HttpStatus.CREATED);
@@ -60,7 +70,7 @@ public class AuthenticationController {
 
     // TODO: need to configure CORS for this endpoint to allow request from anywhere
     @GetMapping(path = "/activate")
-    public ResponseEntity<CommonResponse<RegistrationResponse>> activate(@RequestParam("email")
+    public ResponseEntity<String> activate(@RequestParam("email")
                                                                          @Email(message = "email must a valid email address")
                                                                          @NotBlank(message = "email cannot be blank")
                                                                                  String email,
@@ -73,27 +83,28 @@ public class AuthenticationController {
                 user.getActivationToken()
         );
         CommonResponse<RegistrationResponse> CommonResponse = new CommonResponse<>(HttpStatus.OK.value(), "success", resp);
-        return new ResponseEntity<>(CommonResponse, HttpStatus.OK);
+        String successMsg = "Activation success! Please go back to Peerprep to login: " + authenticationUtil.generateFrontendLandingUrl();
+        return new ResponseEntity<>(successMsg, HttpStatus.OK);
     }
 
     @PostMapping(path = "/password/pre-reset")
     public ResponseEntity<CommonResponse<PasswordPreResetResponse>> preResetPassword(@RequestParam("email")
                                                                                      @Email(message = "email must a valid email address")
                                                                                      @NotBlank(message = "email cannot be blank")
-                                                                                             String email) {
+                                                                                             String email) throws MessagingException {
         String token = authenticationService.generatePasswordToken(email);
         PasswordPreResetResponse resp = new PasswordPreResetResponse(email, token);
 
-        // TODO: uncomment once we figure out how to send email in prod env
         // send email
-        /*
-        String url = AuthenticationUtil.generatePasswordResetUrl(email, token);
-        String subject = AuthenticationUtil.generatePasswordResetEmailSubject();
-        String recipientEmail = email;
-        String senderEmail = AuthenticationUtil.generateEmailSenderAddress();
-        String emailContent = AuthenticationUtil.buildPasswordResetEmailContent(url);
-        emailService.send(subject, recipientEmail, senderEmail, emailContent);
-         */
+        String url = authenticationUtil.generatePasswordResetUrl(email, token);
+        String subject = authenticationUtil.generatePasswordResetEmailSubject();
+        String senderEmail = authenticationUtil.generateEmailSenderAddress();
+        String emailContent = authenticationUtil.buildPasswordResetEmailContent(url);
+
+        if (emailEnabled) {
+            emailService.send(subject, email, senderEmail, emailContent);
+        }
+
         CommonResponse<PasswordPreResetResponse> CommonResponse = new CommonResponse<>(HttpStatus.OK.value(), "success", resp);
         return new ResponseEntity<>(CommonResponse, HttpStatus.OK);
     }

d366a27b6fca6e91086fefc5614100b9ab4fb943
Auxin <dexterzhang430@hotmail.com>
2021-10-25 12:20:40 +0800

    implement websocket

---
+++ peerprep/src/main/java/org/cs3219/project/peerprep/component/WebSocketServer.java
@@ -0,0 +1,90 @@
+package org.cs3219.project.peerprep.component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.cs3219.project.peerprep.model.dto.interview.PeerMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@ServerEndpoint("/interview/{userId}")
+@Component
+@Slf4j
+public class WebSocketServer {
+
+    /**
+     * Keep record of current number of connections
+     */
+    private static final AtomicInteger onlineCount = new AtomicInteger(0);
+
+    /**
+     * Store all online connected server-client
+     */
+    private static final Map<String, WebSocketServer> webSocketServerMap = new ConcurrentHashMap<>();
+
+    private Session session;
+
+    private String userId;
+
+    @Autowired
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("userId") String userId) {
+        this.session = session;
+        this.userId = userId;
+        if (webSocketServerMap.containsKey(userId)) {
+            webSocketServerMap.remove(userId);
+            webSocketServerMap.put(userId, this);
+        } else {
+            webSocketServerMap.put(userId, this);
+            onlineCount.incrementAndGet();
+        }
+        log.info("[WebSocketServer.onOpen] connected user:{}, total online users:{}", userId, onlineCount.get());
+    }
+
+    @OnClose
+    public void onClose() {
+        if (webSocketServerMap.containsKey(userId)) {
+            webSocketServerMap.remove(userId);
+            onlineCount.decrementAndGet();
+        }
+        log.info("[WebSocketServer.onClose] exited user:{}, total online users:{}", userId, onlineCount.get());
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+        log.info("[WebSocketServer.onMessage.MessageSource] userId:{}, message:{}", userId, message);
+        try {
+            if (StringUtils.isNotBlank(message)) {
+                PeerMessage peerMessage = objectMapper.readValue(message, PeerMessage.class);
+                String peerId = peerMessage.getPeerId().toString();
+                if (StringUtils.isNotBlank(peerId) && webSocketServerMap.containsKey(peerId)) {
+                    webSocketServerMap.get(peerId).sendMessage(peerMessage.getData());
+                } else {
+                    log.error("[WebSocketServer.onMessage.PeerNotFound] peerId:{}", peerId);
+                }
+            }
+        } catch (IOException e) {
+            log.error("[WebSocketServer.OnMessage] error: ", e);
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        log.error("[WebSocketServer.onError] userId:{}, error: ", userId, error);
+        error.printStackTrace();
+    }
+
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+}
---
+++ peerprep/src/main/java/org/cs3219/project/peerprep/model/dto/interview/PeerMessage.java
@@ -0,0 +1,11 @@
+package org.cs3219.project.peerprep.model.dto.interview;
+
+import lombok.Data;
+
+@Data
+public class PeerMessage {
+
+    private Long peerId;
+
+    private String data;
+}

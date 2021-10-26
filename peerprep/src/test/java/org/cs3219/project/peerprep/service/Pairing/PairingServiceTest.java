c9cdd83092edda1b9122858ef3fbe6ddf14d39e5
seris370 <1812563232@qq.com>
2021-10-26 13:21:24 +0800

    debug test

--- peerprep/src/test/java/org/cs3219/project/peerprep/service/Pairing/PairingServiceTest.java
+++ peerprep/src/test/java/org/cs3219/project/peerprep/service/Pairing/PairingServiceTest.java
@@ -41,51 +41,54 @@ public class PairingServiceTest {
 
     @Test
     public void testPairing() throws ExecutionException, InterruptedException {
-        ThreadPoolExecutor executor =
-                (ThreadPoolExecutor) Executors.newFixedThreadPool(6);
-        SoftAssertions softly = new SoftAssertions();
-        List<Future<PairingResponse>> futures = new ArrayList<>();
+        synchronized (MatchMaking.class) {
+            ThreadPoolExecutor executor =
+                    (ThreadPoolExecutor) Executors.newFixedThreadPool(6);
+            SoftAssertions softly = new SoftAssertions();
+            List<Future<PairingResponse>> futures = new ArrayList<>();
 
-        for (int i = 0; i < level * groupSize; i++) {
-            PairingRequest request = requests[i];
-            Future<PairingResponse> future = executor.submit(() -> {
-                try {
-                    return pairingService.getPairingResult(request);
-                } catch (InterruptedException e) {
-                    softly.fail("Pairing service interrupted.");
-                    return null;
-                }
-            });
-            futures.add(future);
-        }
-
-        long[] peerIds = new long[level * groupSize];
-
-        for (Future<PairingResponse> future : futures) {
-            PairingResponse response = future.get();
-            long userId = response.getUserId();
-            long peerId = response.getPeerId();
-            int difficulty = response.getDifficulty();
-            int requestPos = (int) (userId - 1);
-            PairingRequest request = requests[requestPos];
-
-            softly.assertThat(userId)
-                    .as("Unmatched userId in request and response")
-                    .isEqualTo(request.getUserId());
-            softly.assertThat(difficulty)
-                    .as("Unmatched difficulty in request and response")
-                    .isEqualTo(request.getDifficulty());
-            softly.assertThat(requests[(int) (peerId - 1)].getDifficulty())
-                    .as("Unmatched difficulty for user and peer.")
-                    .isEqualTo(difficulty);
-
-            if (peerIds[requestPos] == 0) {
-                peerIds[requestPos] = peerId;
-            } else {
-                softly.assertThat(peerIds[requestPos])
-                        .as("User matched to multiple peers.")
-                        .isEqualTo(peerId);
+            for (int i = 0; i < level * groupSize; i++) {
+                PairingRequest request = requests[i];
+                Future<PairingResponse> future = executor.submit(() -> {
+                    try {
+                        return pairingService.getPairingResult(request);
+                    } catch (InterruptedException e) {
+                        softly.fail("Pairing service interrupted.");
+                        return null;
+                    }
+                });
+                futures.add(future);
             }
+
+            long[] peerIds = new long[level * groupSize];
+
+            for (Future<PairingResponse> future : futures) {
+                PairingResponse response = future.get();
+                long userId = response.getUserId();
+                long peerId = response.getPeerId();
+                int difficulty = response.getDifficulty();
+                int requestPos = (int) (userId - 1);
+                PairingRequest request = requests[requestPos];
+
+                softly.assertThat(userId)
+                        .as("Unmatched userId in request and response")
+                        .isEqualTo(request.getUserId());
+                softly.assertThat(difficulty)
+                        .as("Unmatched difficulty in request and response")
+                        .isEqualTo(request.getDifficulty());
+                softly.assertThat(requests[(int) (peerId - 1)].getDifficulty())
+                        .as("Unmatched difficulty for user and peer.")
+                        .isEqualTo(difficulty);
+
+                if (peerIds[requestPos] == 0) {
+                    peerIds[requestPos] = peerId;
+                } else {
+                    softly.assertThat(peerIds[requestPos])
+                            .as("User matched to multiple peers.")
+                            .isEqualTo(peerId);
+                }
+            }
+            softly.assertAll();
         }
     }
 }
